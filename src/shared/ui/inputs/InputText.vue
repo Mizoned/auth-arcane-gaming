e45275af3e6287a9beabcfecc8f42887d4e7d87e
Valery Shcherbinin <fundroin@mail.ru>
2024-10-26 11:47:22 +0500

    Добавляет компоненты InputText и FloatLabel

---
+++ src/shared/ui/inputs/InputText.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface InputTextProps {
+  fluid?: boolean
+}
+
+const modelValue = defineModel<string>();
+
+defineProps<InputTextProps>();
+</script>
+
+<template>
+  <input
+    :class="[
+      'input-text',
+      { 'is-filled': modelValue?.length },
+      { 'is-fluid': fluid }
+    ]"
+    v-model="modelValue"
+    type="text"
+  >
+</template>
+
+<style lang="scss">
+.input-text {
+  padding: calc(1rem - 1px);
+  border: 1px solid var(--ag-champagne-color);
+  line-height: 1.5rem;
+  border-radius: var(--ag-border-radius);
+  outline: none;
+
+  &.is-fluid {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/shared/ui/labels/FloatLabel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="float-label">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.float-label {
+  position: relative;
+
+  label {
+    position: absolute;
+    pointer-events: none;
+    top: 50%;
+    left: 1rem;
+    transform: translateY(-50%);
+    color: var(--ag-light-grey-color);
+    line-height: 1.5rem;
+    transition: all ease 0.2s; ;
+  }
+
+  &:has(input:focus) label, &:has(input.is-filled) label {
+    top: 0;
+    background-color: var(--ag-white-color);
+    padding: 0.188rem;
+    font-size: var(--ag-font-size-s);
+  }
+}
+</style>
