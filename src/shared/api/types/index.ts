199b6c7c34fba106b834c1fec634f2fafa0a336a
Valery Shcherbinin <fundroin@mail.ru>
2024-10-28 08:33:14 +0500

    Удаляет лишние файлы

--- src/shared/api/types/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Channel } from '@/entities/channels';
-
-export interface CreateSessionResponse {
-  channels: Channel[];
-  sent_to: string;
-  session_expired_at: string;
-  session_id: string;
-}
